c2ebb91be4e77864d58a54b678eaccc4dad699ec
VIKRAM-PAREEK <42038108+VIKRAM-PAREEK@users.noreply.github.com>
2020-01-29 15:40:45 +0530

    Adding the gaming zone i.e the Grid

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+class App extends Component{
+render(){
   return (
-    <h1>TIC TAC TOE</h1>
-    );
+    <div id="game">
+          <h1>TIC TAC TOE</h1>
+          <h3>Player 1:X Player 2:O</h3>
+        <div id="board" align="center">
+          <button id="1"></button>
+          <button id="2"></button>
+          <button id="3"></button>
+          <br></br>
+          <button id="4"></button>
+          <button id="5"></button>
+          <button id="6"></button>
+          <br></br>
+          <button id="7"></button>
+          <button id="8"></button>
+          <button id="9"></button>
+        </div>
+    </div>
+      );
 }
-
+}
 export default App;
