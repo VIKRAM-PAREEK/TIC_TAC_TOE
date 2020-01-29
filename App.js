03022dda1467ad85b87f1bed4c46b14c4d51877c
VIKRAM-PAREEK <42038108+VIKRAM-PAREEK@users.noreply.github.com>
2020-01-29 16:53:33 +0530

    Alternating the moves of player X & player Y

--- App.js
+++ App.js
@@ -2,23 +2,33 @@ import React from 'react';
 import './App.css';
 
 class App extends Component{
-render(){
+  constructor(){
+    super();
+    this.state={
+      turn: 'X'
+    }
+  }
+  clicked(event){
+      event.target.innerText = this.state.turn
+      this.setState({
+        turn: this.state.turn === 'X'?'O':'X'
+    })
+  }
+  render(){
   return (
     <div id="game">
           <h1>TIC TAC TOE</h1>
           <h3>Player 1:X Player 2:O</h3>
-        <div id="board" align="center">
-          <button id="1"></button>
-          <button id="2"></button>
-          <button id="3"></button>
-          <br></br>
-          <button id="4"></button>
-          <button id="5"></button>
-          <button id="6"></button>
-          <br></br>
-          <button id="7"></button>
-          <button id="8"></button>
-          <button id="9"></button>
+        <div id="board" align="center" onClick={(e)=>this.clicked(e)}>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
         </div>
     </div>
       );
